7517812115af6131c56c4dd258218b0ec5fe983d
JustinMartin7x <justin.martin7x@gmail.com>
2020-10-19 14:52:40 -0700

    added Javascript start

--- app.js
+++ app.js
@@ -3,3 +3,14 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+
+const dialog = document.getElementById('dialog');
+const name = document.querySelector('input');
+const diceOne = document.getElementById('diceOne');
+const diceTwo = document.getElementById('diceTwo');
+const diceThree = document.getElementById('diceThree');
+const diceFour = document.getElementById('diceFour');
+
+
+
