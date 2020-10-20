a33dd6bffabba462a68d5999bb91d9df6ae47150
JustinMartin7x <justin.martin7x@gmail.com>
2020-10-19 18:16:15 -0700

    jSS stuff and things

--- data/wizard-Script.js
+++ data/wizard-Script.js
@@ -1,28 +1,37 @@
+
+import { getFromLocalStorage } from '../utils.js';
+let userData = getFromLocalStorage('USER');
+if (!userData) {
+    userData = {
+        name: 'Traveler',
+    };
+
+}
 export default [
     {
         id: '001',
         title: 'Welcome',
-        
-        description: 
-        `Welcome young adventurer! What is your name?`        
+
+        description:
+            `Welcome young adventurer! What is your name?`
     },
     {
         id: '002',
         title: 'Nice to Meet you',
         description:
-        `Nice to make your acquaintance ${name}! Welcome to "The Adventurers Guild Character Creation Page" my name is Archion Urasus!`
+            `Nice to make your acquaintance ${userData.name}! Welcome to "The Adventurers Guild Character Creation Page" my name is Archion Urasus!`
     },
     {
         id: '003',
         title: 'Intro To D&D',
         description:
-        `Dungeons and Dragons is a wonderful game that expands your imagination! The first step to any Great campaign is character creation!
+            `Dungeons and Dragons is a wonderful game that expands your imagination! The first step to any Great campaign is character creation!
         allow me to guid you through the process`
     },
     {
         id: '004',
         title: 'The Process',
-        description: 
-        `can you see the four dice below me? ok so we must first roll all of the dice, however we are going to remove the lowest of the dice and keep the
+        description:
+            `can you see the four dice below me? ok so we must first roll all of the dice, however we are going to remove the lowest of the dice and keep the
         sum of the highest three! we are going to do this 6 times. Don't worry I'll keep track of your rolls`
     }];
