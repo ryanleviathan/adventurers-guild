eaaff937dc86611c789b0e965db9e9eea773e91d
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-20 16:51:02 -0700

    drop downs populated

--- character-sheet/character-app.js
+++ character-sheet/character-app.js
@@ -1,5 +1,8 @@
 import { findById, getFromLocalStorage, setInLocalStorage, USER } from '../utils.js';
 
+// Pull from localStorage
+const userdata = getFromLocalStorage(USER);
+const rolls = userdata.rolls;
 
 //Attributes from DOM
 const strength = document.getElementById('str');
@@ -14,20 +17,48 @@ const wisdom = document.getElementById('wis');
 const wisdomMod = document.getElementById('wisMod');
 const charisma = document.getElementById('cha');
 const charismaMod = document.getElementById('chaMod');
-//
+
+//drop down options
+const strOptions = document.getElementById('strOptions');
+const dexOptions = document.getElementById('dexOptions');
+const conOptions = document.getElementById('conOptions');
+const intOptions = document.getElementById('intOptions');
+const wisOptions = document.getElementById('wisOptions');
+const chaOptions = document.getElementById('chaOptions');
+
 // AC Initiative and Speed from DOM
-const armorClass = document.getElementById(armorClass);
+const armorClass = document.getElementById('ac');
 const initiative = document.getElementById('initiative');
 const speed = document.getElementById('speed');
-//
+
 // roll display
 const rollOne = document.getElementById('rollOne');
+rollOne.textContent = userdata.rolls[0];
 const rollTwo = document.getElementById('rollTwo');
+rollTwo.textContent = userdata.rolls[1];
 const rollThree = document.getElementById('rollThree');
+rollThree.textContent = userdata.rolls[2];
 const rollFour = document.getElementById('rollFour');
+rollFour.textContent = userdata.rolls[3];
 const rollFive = document.getElementById('rollFive');
+rollFive.textContent = userdata.rolls[4];
 const rollSix = document.getElementById('rollSix');
+rollSix.textContent = userdata.rolls[5];
 
-const userdata = getFromLocalStorage(USER);
+//albilty modifier setting
+function populateRolls(select, rolls) {
+    for (let i = 0; i < rolls.length; i++) {
+        const option = document.createElement('option');
+        console.log(option);
+        option.textContent = rolls[i];
+        option.value = rolls[i];
+        select.append(option);
+    }
+}
 
-
+populateRolls(strOptions, rolls);
+populateRolls(dexOptions, rolls); 
+populateRolls(conOptions, rolls); 
+populateRolls(intOptions, rolls); 
+populateRolls(wisOptions, rolls); 
+populateRolls(chaOptions, rolls);  
