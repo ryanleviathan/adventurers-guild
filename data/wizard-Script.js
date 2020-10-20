d9b0ae5c33077dcdcf959df35a2b5563d3566d41
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-20 14:50:01 -0700

    some small tweaks throughout

--- data/wizard-Script.js
+++ data/wizard-Script.js
@@ -1,4 +1,3 @@
-
 import { getFromLocalStorage } from '../utils.js';
 let userData = getFromLocalStorage('USER');
 if (!userData) {
@@ -19,25 +18,23 @@ export default [
         id: '002',
         title: 'Nice to Meet you',
         description:
-            `Nice to make your acquaintance ${userData.name}! Welcome to "The Adventurers Guild Character Creation Page" my name is Archion Urasus!`
+            `Nice to make your acquaintance ${userData.name}! Welcome to The Adventurers Guild Character Creation Page! My name is Archion Urasus.`
     },
     {
         id: '003',
         title: 'Intro To D&D',
         description:
-            `Dungeons and Dragons is a wonderful game that expands your imagination! The first step to any Great campaign is character creation!
-        allow me to guid you through the process`
+            `Dungeons and Dragons is a wonderful game that expands your imagination! The first step to any great campaign is character creation! I'm here to guide you through the process.`
     },
     {
         id: '004',
         title: 'The Process',
         description:
-            `can you see the four dice below me? ok so we must first roll all of the dice, however we are going to remove the lowest of the dice and keep the
-        sum of the highest three! we are going to do this 6 times. Don't worry I'll keep track of your rolls`
+            `Can you see the four dice below me? Let's roll all of the dice! We'll remove the lowest of the dice rolls, keeping the sum of the highest three! We'll do this 6 times to generate 6 stats. Don't worry, I'll keep track of your rolls and fill you in on how to use these numbers later.`
     },
     {
         id: '005',
         title: 'the rolls',
-        description: 'very good! now that we have these number we can move on to the next step!'
+        description: 'Very good! Now that we have these number we can move on to the next step!'
 
     }];
