58aaab40a8cce83482243ea4f28af0b58e43e594
JustinMartin7x <justin.martin7x@gmail.com>
2020-10-21 10:03:28 -0700

    utils for cahr page

---
+++ character-sheet/character-utils.js
@@ -0,0 +1,31 @@
+
+
+
+export function modifier(attribute) {
+    const modVal = Number(attribute.textcontent);
+    if (modVal === 1) {
+        return -5;
+    } if (modVal === 2 || modVal === 3) {
+        return -4;
+    } if (modVal === 4 || modVal === 5) {
+        return -3;
+    } if (modVal === 6 || modVal === 7) {
+        return -2;
+    } if (modVal === 8 || modVal === 9) {
+        return -1;
+    } if (modVal === 10 || modVal === 11) {
+        return 0;
+    } if (modVal === 12 || modVal === 13) {
+        return 1;
+    } if (modVal === 14 || modVal === 15) {
+        return 2;
+    } if (modVal === 16 || modVal === 17) {
+        return 3;
+    } if (modVal === 18 || modVal === 19) {
+        return 4;
+    } if (modVal === 20 || modVal === 21) {
+        return 5;
+    } if (modVal === 22 || modVal === 23) {
+        return 6;
+    }
+}
